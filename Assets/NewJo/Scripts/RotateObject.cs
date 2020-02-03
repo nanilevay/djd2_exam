def7c9c1b96a09401fa27c4b62684434bd7bdd55
nanilev <dianuski99@gmail.com>
2020-02-03 19:52:50 +0000

    add effects and fixes on scene

--- Assets/NewJo/Scripts/RotateObject.cs
+++ Assets/NewJo/Scripts/RotateObject.cs
@@ -9,29 +9,26 @@ public class RotateObject : MonoBehaviour
     Vector3 mPrevPosition = Vector3.zero;
     Vector3 mCurPosition = Vector3.zero;
 
-    public GameObject self;
-
     private float zoom = 80f;
 
     // Update is called once per frame
     void Update()
     {
-
         if(Input.GetMouseButton(0))
         {
             mCurPosition = Input.mousePosition - mPrevPosition;
 
             if (Vector3.Dot(transform.up, Vector3.up) >=0)  //it stops right and left from inverting if object rotated upsidedown
             {
-                self.transform.Rotate(transform.up, -Vector3.Dot(mCurPosition, Camera.main.transform.right), Space.World);  //rotates around Y axis (right/left); Space.World - Rotate object in world space
+                this.transform.Rotate(transform.up, -Vector3.Dot(mCurPosition, Camera.main.transform.right), Space.World);  //rotates around Y axis (right/left); Space.World - Rotate object in world space
             }
 
             else
             {
-                self.transform.Rotate(transform.up, Vector3.Dot(mCurPosition, Camera.main.transform.right), Space.World);  //rotates around Y axis (right/left)
+                this.transform.Rotate(transform.up, Vector3.Dot(mCurPosition, Camera.main.transform.right), Space.World);  //rotates around Y axis (right/left)
             }
 
-            self.transform.Rotate(Camera.main.transform.right, Vector3.Dot(mCurPosition, Camera.main.transform.up), Space.World);  //rotates around up/down
+            this.transform.Rotate(Camera.main.transform.right, Vector3.Dot(mCurPosition, Camera.main.transform.up), Space.World);  //rotates around up/down
         }
 
         mPrevPosition = Input.mousePosition;
